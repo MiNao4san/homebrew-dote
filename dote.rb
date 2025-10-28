44f571d31bfc36c8bf840992ceaf80ce9887ac54
MiNao4san <kmc2538@kamiyama.ac.jp>
2025-10-28 11:42:42 +0900

    version "1.0.1"

--- dote.rb
+++ dote.rb
@@ -2,8 +2,8 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/MiNao4san/homebrew-dote"
   url "https://raw.githubusercontent.com/MiNao4san/homebrew-dote/main/dote.c"
-  sha256 "f86f920e94e6d9e7340d5ce0ed37dc7802709a740b90d0de0798404442a33e30"
-  version "1.0.0"
+  sha256 "366006e9023611290dad49b3cf77fe2546037fc753d17f79db50ac9cf2e9b6d0"
+  version "1.0.1"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
